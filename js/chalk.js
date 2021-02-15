d6dd35c405aa88ac4e1c31684d365057ff7a98ad
signeodelbo <78245086+signeodelbo@users.noreply.github.com>
2021-02-14 22:08:47 -0600

    commit

---
+++ js/chalk.js
@@ -0,0 +1,10 @@
+function popup_alert(){
+    alert("Do you have a death wish?");
+}
+
+function last_alphabetically(){
+    var newArray = ["Cat","Pineapple","River","Factory","Rug","Zurg"];
+    var sort = newArray.sort();
+    var reverse = sort.reverse();
+    console.log(reverse[0]);
+}
